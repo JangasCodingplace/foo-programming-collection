4718eb7feb2bf1110939e10a9bc5805f42fbdd25
JangasCodingplace <janisgoesser92@gmail.com>
2023-04-07 18:20:09 +0200

    Write row generator

--- csv-customer-file-generator/python-simple-approach/csv_customer_file_generator/main.py
+++ csv-customer-file-generator/python-simple-approach/csv_customer_file_generator/main.py
@@ -1,11 +1,26 @@
 import argparse
 from pathlib import Path
 from dataclasses import dataclass
+from random import randint
+from datetime import datetime, timedelta
 
 
 @dataclass(frozen=True)
 class CommandLineArgs:
     target: Path
+    article_count: int
+    customer_count: int
+    min_date: datetime
+    max_date: datetime
+    row_count: int
+
+
+@dataclass
+class CustomerOrder:
+    customer_id: int
+    article_id: int
+    order_id: int
+    timestamp: int
 
 
 def parse_command_line_args() -> CommandLineArgs:
@@ -14,17 +29,103 @@ def parse_command_line_args() -> CommandLineArgs:
         '--target',
         type=str,
         help='Target directory for generated csv file',
-        default=str(Path(__file__).parent / "output")
+        default=str(Path(__file__).parent / "output"),
+    )
+    parser.add_argument(
+        '--article-count',
+        type=int,
+        help='Number of different article numbers which get generated',
+        default=100,
+    )
+    parser.add_argument(
+        '--customer-count',
+        type=int,
+        help='Number of different customers which get generated',
+        default=100,
+    )
+    parser.add_argument(
+        '--min-date',
+        type=str,
+        help='min-date for order generation. Should have format YYYY-mm-dd',
+        default="2022-01-01",
+    )
+    parser.add_argument(
+        '--max-date',
+        type=str,
+        help='max-date for order generation. Should have format YYYY-mm-dd',
+        default="2023-01-01",
+    )
+    parser.add_argument(
+        '--row-count',
+        type=int,
+        help='row count for order generation.',
+        default=10000,
     )
 
     args = parser.parse_args()
     return CommandLineArgs(
-        target=Path(args.target)
+        target=Path(args.target),
+        article_count=args.article_count,
+        customer_count=args.customer_count,
+        min_date=datetime.strptime(args.min_date, "%Y-%m-%d"),
+        max_date=datetime.strptime(args.max_date, "%Y-%m-%d"),
+        row_count=args.row_count,
     )
+
+
+class Generator:
+    def __init__(
+        self,
+        article_count: int,
+        customer_count: int,
+        min_date: datetime,
+        max_date: datetime
+    ):
+        self.articles = [i for i in range(10**5, 10**5 + article_count)]
+        self.customers = [i for i in range(10 ** 3, 10 ** 3 + customer_count)]
+        self.date_list = [min_date + timedelta(days=d) for d in range((max_date - min_date).days)]
+
+    def _generate_order(self, order_id: int) -> list[CustomerOrder]:
+        date = self.date_list[randint(0, len(self.date_list)-1)]
+        timestamp = datetime(
+            year=date.year,
+            month=date.month,
+            day=date.day,
+            hour=randint(0, 23),
+            minute=randint(0, 59),
+            second=randint(0, 59),
+        )
+        customer = self.customers[randint(0, len(self.customers)-1)]
+        item_count = randint(1, 20)
+        articles = [self.articles[randint(0, len(self.articles)-1)] for _ in range(item_count)]
+        return [
+            CustomerOrder(
+                customer_id=customer,
+                article_id=article,
+                order_id=order_id,
+                timestamp=int(timestamp.timestamp()),
+            ) for article in articles
+        ]
+
+    def generate_rows(self, max_row_count: int) -> list[CustomerOrder]:
+        order_id = 1000
+        rows = []
+        while len(rows) < max_row_count:
+            order = self._generate_order(order_id)
+            rows += order
+            order_id += 1
+        return rows
 
 
 def main():
     command_line_args = parse_command_line_args()
+    generator = Generator(
+        article_count=command_line_args.article_count,
+        customer_count=command_line_args.customer_count,
+        min_date=command_line_args.min_date,
+        max_date=command_line_args.max_date,
+    )
+    rows = generator.generate_rows(command_line_args.row_count)
     print()
 
 

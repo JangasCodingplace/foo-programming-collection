8ef2de24376eedab960a3efca38a80954aeb893e
JangasCodingplace <janisgoesser92@gmail.com>
2023-04-13 00:36:42 +0200

    implement cors origin

--- light-gps-tracker-microservice/basic-api-fastapi/clients/react-web-client/src/App.js
+++ light-gps-tracker-microservice/basic-api-fastapi/clients/react-web-client/src/App.js
@@ -1,14 +1,29 @@
-import { MapContainer, Marker, Popup, TileLayer } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import { MapContainer, Marker, TileLayer } from "react-leaflet";
 import { Icon } from "leaflet";
 import "./App.css"
 import "leaflet/dist/leaflet.css";
 
 
 function App() {
+  const [position, setPosition] = useState({ lat: 52.517304, long: 13.407008 });
+
   const myIcon = new Icon({
     iconUrl: "https://cdn-icons-png.flaticon.com/512/744/744465.png",
     iconSize: [80, 80]
   })
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      axios.get("http://127.0.0.1:8000/get-position/40ad23c6-5072-460f-9d1d-acddff5472fa").then((response) => {
+        setPosition({ lat: response.data.lat, long: response.data.long });
+      });
+    }, 5000);
+
+    return () => clearInterval(interval);
+  }, []);
+
 
   return (
     <div>
@@ -17,7 +32,7 @@ function App() {
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       />
-      <Marker position={[52.573732,13.209456]} icon={myIcon}/>
+      <Marker position={[position.lat, position.long]} icon={myIcon}/>
     </MapContainer>
     </div>
   );
--- light-gps-tracker-microservice/basic-api-fastapi/light_gps_tracker/main.py
+++ light-gps-tracker-microservice/basic-api-fastapi/light_gps_tracker/main.py
@@ -1,5 +1,20 @@
-import uvicorn
+from fastapi.middleware.cors import CORSMiddleware
 import views
 
 
 app = views.app
+
+origins = [
+    "http://localhost",
+    "http://localhost:3000",
+    "http://127.0.0.1",
+    "http://127.0.0.1:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)

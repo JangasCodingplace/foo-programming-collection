95829a8a0576fc751077b64faf24b82427a63bfa
JangasCodingplace <janisgoesser92@gmail.com>
2023-04-12 22:44:50 +0200

    use connection pool for db

--- light-gps-tracker-microservice/basic-api-fastapi/light_gps_tracker/main.py
+++ light-gps-tracker-microservice/basic-api-fastapi/light_gps_tracker/main.py
@@ -1,5 +1,7 @@
 import os
 import psycopg2
+import psycopg2.pool
+from typing import Type
 import uuid
 from fastapi import FastAPI
 from datetime import datetime
@@ -17,32 +19,51 @@ class VehiclePosition(BaseModel):
     speed: int
 
 
+TIMESCALE_CONNECTION_POOL = psycopg2.pool.SimpleConnectionPool(
+    minconn=1,
+    maxconn=10,
+    user=os.getenv("TIMESCALEDB_USER", "rootuser"),
+    password=os.getenv("TIMESCALEDB_PASSWORD", "rootpassword"),
+    database=os.getenv("TIMESCALEDB_DATABASE", "my_delivery"),
+    host=os.getenv("TIMESCALEDB_HOST", "localhost"),
+    port=os.getenv("TIMESCALEDB_PORT", "5432"),
+)
+
+
+class TimeScaleHandler:
+    def __init__(self, pool: psycopg2.pool):
+        self.pool = pool
+
+    def __enter__(self):
+        self.conn = self.pool.getconn()
+        self.conn.autocommit = True
+        self.cursor = self.conn.cursor()
+        return self
+
+    def __exit__(self, *args, **kwargs):
+        self.cursor.close()
+        self.pool.putconn(self.conn)
+        return
+
+    def execute(self, query: str, *args):
+        return self.cursor.execute(query, *args)
+
+
 class VehiclePositionRepository:
     @staticmethod
     def create(vehicle_position: VehiclePosition):
-        conn = psycopg2.connect(
-            user=os.getenv("TIMESCALEDB_USER", "rootuser"),
-            password=os.getenv("TIMESCALEDB_PASSWORD", "rootpassword"),
-            database=os.getenv("TIMESCALEDB_DATABASE", "my_delivery"),
-            host=os.getenv("TIMESCALEDB_HOST", "localhost"),
-            port=os.getenv("TIMESCALEDB_PORT", "5432"),
-        )
-        cursor = conn.cursor()
         query = """
             INSERT INTO vehicle_position (timestamp, vehicle_id, lat, long, speed)
             VALUES (%s, %s, %s, %s, %s)
         """
-        cursor.execute(query, (
-            datetime.fromtimestamp(vehicle_position.timestamp),
-            str(vehicle_position.vehicle_id),
-            vehicle_position.lat,
-            vehicle_position.long,
-            vehicle_position.speed,
-        ))
-        conn.commit()
-
-        cursor.close()
-        conn.close()
+        with TimeScaleHandler(TIMESCALE_CONNECTION_POOL) as db_handler:
+            db_handler.execute(query, (
+                datetime.fromtimestamp(vehicle_position.timestamp),
+                str(vehicle_position.vehicle_id),
+                vehicle_position.lat,
+                vehicle_position.long,
+                vehicle_position.speed,
+            ))
 
 
 @app.get("/get-position/{vehicle_id}")
